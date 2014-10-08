c29cd79a10db58b28bb3b9694541b3e3ef0b7156
miton <rcd18@hotmail.fr>
2014-10-08 17:58:34 +0200

    coucou

--- site/index.php
+++ site/index.php
@@ -7,6 +7,7 @@
 
 		<?php
 			require_once( 'include/limonade.php' );
+			require_once( 'include/bdd.php');
 			option('limonade_dir', 'include/'); // this fiolder contains the limonade.php main file
 
 			dispatch('/clean_install', 'clean_install');
--- site/views/renseignement.html.php
+++ site/views/renseignement.html.php
@@ -1,3 +1,17 @@
-<?php
-	echo $mail;
-?>
+<?php content_for('header'); ?>
+<img src="../images/logo_ISEN.png" align="center">
+<?php end_content_for();?>
+
+<?php content_for('body'); ?>
+
+<?php 
+	print_r( query('SELECT * FROM data') ); ?>
+
+
+
+
+
+
+
+
+<?php end_content_for(); ?>
