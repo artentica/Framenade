c97f1667bccc88484be7ba4f41308a6e489ebbc7
miton <rcd18@hotmail.fr>
2014-10-08 16:58:26 +0200

    ok miton

--- site/controllers/bdd.php
+++ site/controllers/bdd.php
@@ -1,14 +1,41 @@
 <?php
+		$host      = dbhost;
+    	$user      = dbuser;
+    	$pass      = dbpass;
+    	$dbname    = dbname;
 
-	/**
-	* 
-	*/
-	class bdd extends PDO
-	{
-		
-		function __construct(argument)
+    	$reponse   = array();
+
+		function connect()
 		{
-			# code...
+			// Set DSN
+	        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+	        // Set options
+	        $options = array(
+	            PDO::ATTR_PERSISTENT    => true,
+	            PDO::ATTR_ERRMODE       => PDO::ERRMODE_EXCEPTION
+	        );
+	        // Create a new PDO instanace
+	        try{
+	            $db = new PDO($dsn, $this->user, $this->pass, $options);
+	        }
+	        // Catch any errors
+	        catch(PDOException $e){
+	            $this->error = $e->getMessage();
+	        }
+	    }
+
+		function query( $str )
+		{
+			connect();
+	    	$temp = $db->query( $str );
+	    	$this->reponse = $temp->fetchAll();
+	    	$temp->closeCursor();
+	    	return $reponse;
 		}
-	}
+		function exec( $str )
+		{
+			connect();
+	    	return $db->exec( $str );
+		}
 ?>
--- site/index.php
+++ site/index.php
@@ -3,16 +3,17 @@
 
 	</head>
 	<body>
+		<?php
+			$test = query('SELECT * FROM data');
+			print_r($test);
+		?>
 		PAGE D ACCUEIl
 
 		<?php
 			require_once( 'include/limonade.php' );
 			option('limonade_dir', 'include/'); // this fiolder contains the limonade.php main file
 
-			dispatch('/', 'login');
 			dispatch('/clean_install', 'clean_install');
-
-			
 
 			run();
 		?>
